372be70b2c3d2780e2af03cbf66286590ef1d795
kapil-panchal <kapil.panchal@nouvelletechnology.com.au>
2025-06-03 07:33:58 +0530

    [init] helloworld gradle

---
+++ gradle-helloworld-1/src/test/java/com/java/gradle/GradleHelloworld1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleHelloworld1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ hello-world-1/src/com/java/hello/HelloWorld1.java
@@ -0,0 +1,7 @@
+package com.java.hello;
+
+public class HelloWorld1 {
+	public static void main(String[] args) {
+		System.err.println("HELLO WORLD");
+	}
+}
