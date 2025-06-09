144b6064aed53752385e360b4cdc156245a53038
kapil-panchal <kapil.panchal@nouvelletechnology.com.au>
2025-06-09 09:39:36 +0530

    [refactor] added

---
+++ cqrs-command-query-responsibility-segregation-design-pattern-3/src/test/java/com/java/cqrs/CqrsCommandQueryResponsibilitySegregationDesignPattern3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.cqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsCommandQueryResponsibilitySegregationDesignPattern3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
