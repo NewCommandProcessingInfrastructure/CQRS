842cef87458d01d5671f67df31a5e15dc9b078ee
kapil-panchal <kkapil.panchal@gmail.com>
2025-07-27 17:29:32 +0530

    [validation] added more

--- DemoValidation/src/main/java/com/java/validation/DemoValidation/api/UserController.java
+++ DemoValidation/src/main/java/com/java/validation/DemoValidation/api/UserController.java
@@ -1,7 +1,9 @@
 package com.java.validation.DemoValidation.api;
 
+import com.java.validation.DemoValidation.data.CurrencyCreateRequest;
 import com.java.validation.DemoValidation.data.UserRequest;
 import jakarta.validation.Valid;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,8 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
   @PostMapping
   public ResponseEntity<String> createUser(@Valid @RequestBody UserRequest userRequest) {
     return ResponseEntity.status(HttpStatus.OK).body("User is valid!");
   }
+
+  @PostMapping(path = "/currency")
+  public ResponseEntity<String> createCurrency(@Valid @RequestBody CurrencyCreateRequest request) {
+    return ResponseEntity.status(HttpStatus.OK).body("Currency Created");
+  }
+
 }
---
+++ DemoValidation/src/main/java/com/java/validation/DemoValidation/data/CurrencyCreateRequest.java
@@ -0,0 +1,40 @@
+package com.java.validation.DemoValidation.data;
+
+import jakarta.validation.constraints.*;
+import java.io.Serial;
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CurrencyCreateRequest implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = 1L;
+
+  @NotBlank(message = "{currency.code.notBlank}")
+  @Pattern(regexp = "^[A-Z]{3}$", message = "{currency.code.invalidFormat}")
+  private String code;
+
+  @NotBlank(message = "{currency.name.notBlank}")
+  private String name;
+
+  @NotNull(message = "{currency.decimalPlaces.notNull}")
+  @Min(value = 0, message = "{currency.decimalPlaces.min}")
+  @Max(value = 5, message = "{currency.decimalPlaces.max}")
+  private Integer decimalPlaces;
+
+  @Min(value = 0, message = "{currency.inMultiplesOf.min}")
+  @Max(value = 1000, message = "{currency.inMultiplesOf.max}")
+  private Integer inMultiplesOf;
+
+  @NotBlank(message = "{currency.displaySymbol.notBlank}")
+  private String displaySymbol;
+
+  @NotBlank(message = "{currency.nameCode.notBlank}")
+  @Pattern(regexp = "^[A-Za-z]+\\.[A-Z]{3}$", message = "{currency.nameCode.invalidFormat}")
+  private String nameCode;
+}
