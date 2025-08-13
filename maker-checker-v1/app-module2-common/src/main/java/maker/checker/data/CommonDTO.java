cfb65245455d619617a522e053da00dcfb8e0fe9
kapil-panchal <kkapil.panchal@gmail.com>
2025-08-13 16:27:24 +0530

    [refactor] added validations

--- maker-checker-v1/app-module2-common/src/main/java/maker/checker/data/CommonDTO.java
+++ maker-checker-v1/app-module2-common/src/main/java/maker/checker/data/CommonDTO.java
@@ -1,10 +1,14 @@
 package maker.checker.data;
 
 import java.io.Serializable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @NoArgsConstructor
@@ -12,7 +16,15 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CommonDTO implements Serializable {
 
+  @NotNull
+  @PositiveOrZero
   private Long id;
+
+  @NotBlank
+  @Length(min = 5, max = 100)
   private String firstName;
+
+  @NotBlank
+  @Length(min = 5, max = 100)
   private String lastName;
 }
