4079a0ee027ef8e83630f267f19671a1801ece20
kapil-panchal <root@0.0.8.156>
2025-10-02 10:50:53 +0530

    [init] initialized new project cqrs2 with pre and post process

---
+++ cqrs-new-command-infrastructure-2/src/main/java/com/java/workflow/CqrsNewCommandInfrastructure2Application.java
@@ -0,0 +1,13 @@
+package com.java.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsNewCommandInfrastructure2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsNewCommandInfrastructure2Application.class, args);
+	}
+
+}
---
+++ cqrs-new-command-infrastructure-2/src/test/java/com/java/workflow/CqrsNewCommandInfrastructure2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsNewCommandInfrastructure2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
