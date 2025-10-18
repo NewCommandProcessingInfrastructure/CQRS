08837688e90572d2be238df0716636ea78dc5207
kapil-panchal <kkapil.panchal@gmail.com>
2025-10-18 13:03:41 +0530

    [refactor] removed unused postProcess

--- cqrs-new-command-infrastructure-2/src/main/java/com/java/workflow/infrastructure/core/CommandMiddleware.java
+++ cqrs-new-command-infrastructure-2/src/main/java/com/java/workflow/infrastructure/core/CommandMiddleware.java
@@ -2,5 +2,4 @@ package com.java.workflow.infrastructure.core;
 
 public interface CommandMiddleware {
   void invoke(Command<?> command);
-  default void postProcess(Command<?> command, Object result, Throwable exception) {}
 }
